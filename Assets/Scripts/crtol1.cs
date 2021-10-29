316a8891723e3206b5754559d0267543ad3e1d98
jcarlos5 <josecarlosvilchezvillegas@gmail.com>
2021-10-28 22:39:01 -0500

    finalizado

--- Assets/Scripts/crtol1.cs
+++ Assets/Scripts/crtol1.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class crtol1 : MonoBehaviour
 {
     private Animator ani;
+    public int vidas=5;
+    public Text txtMsj;
+    public Text txtVidas;
 
     void Start () {
         ani = GetComponent<Animator> ();
+        txtVidas.text = "Vidas : " + vidas;
     }
         
     void Update () {
@@ -17,6 +22,26 @@ public class crtol1 : MonoBehaviour
 
         transform.Rotate(0,r,0);
         ani.SetFloat("velocidad",v);
+
+        if(vidas <= 0)
+        {
+            txtMsj.text = "Se te acabaron las vidas";
+            GetComponent<crtol1>().enabled = false;
+            ani.SetBool("isMorido", true);
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if(obj.gameObject.tag == "Enemy")
+        {
+            if(vidas > 0)
+            {
+                vidas--;
+                txtVidas.text = "Vidas : " + vidas;
+            }
+        }
     }
 
 }
